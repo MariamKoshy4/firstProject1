2ad19ef8c49c744fb3b5f54c8ae704d53502cb67
MariamKoshy4 <mariamkoshy8@gmail.com>
2024-05-27 18:03:29 -0500

    first commit

---
+++ src/test/java/variousConcepts/LearnLogin.java
@@ -0,0 +1,89 @@
+package variousConcepts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LearnLogin {
+
+	WebDriver driver; // Global Variable - Object variable.
+
+	@Before // Setup
+	public void init() {
+
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.get("https://codefios.com/ebilling/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// These much needed for our init method
+
+	}
+
+	@Test
+	public void testLogin() throws InterruptedException {
+
+		// type name = value
+		// Element list - WebElement
+		// Asking the driver to find the WebElement and store that
+		WebElement USER_NAME_ELEMENT = driver.findElement(By.xpath("//*[@id=\"user_name\"]"));
+		WebElement PASSWORD_ELEMENT = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+		WebElement SIGNIN_BUTTON_ELEMENT = driver.findElement(By.xpath("//*[@id=\"login_submit\"]"));
+
+		// Element list - By Type
+		// It is like String Variable in Java
+		// Its like a Local/Regular Variable we are creating. Thats why the driver is
+		// not involved in it and not going crazy.
+		By USER_NAME_FIELD = By.xpath("//*[@id=\"user_name\"]");
+		By PASSWORD_FIELD = By.xpath("//*[@id=\"password\"]");
+		By SIGNIN_BUTTON_FIELD = By.xpath("//*[@id=\"login_submit\"]");
+		By DASHBOARD_HEADER_FIELD = By.xpath("//strong[text()='Dashboard']");
+
+		// driver.findElement(By.xpath("//*[@id=\"user_name\"]")).sendKeys("demo@codefios.com");
+		// driver.findElement(USER_NAME_FIELD).sendKeys("demo@codefios.com");
+	
+		
+		
+		// Thread.sleep(2000);
+		// USER_NAME_ELEMENT.clear();
+
+		USER_NAME_ELEMENT.sendKeys("demo@codefios.com");
+		PASSWORD_ELEMENT.sendKeys("abc123");
+		SIGNIN_BUTTON_ELEMENT.click();
+
+		WebElement DASHBOARD_HEADER_ELEMENT = driver.findElement(By.xpath("//strong[text()='Dashboard']"));
+		Assert.assertTrue("DashBoard page not found!!", DASHBOARD_HEADER_ELEMENT.isDisplayed());
+		
+		//Till Here Code is.
+		// -------->>>ACTUAL CODE IS TILL ABOVE--------->>>
+
+		// --------->>>below are just examples for Exceptions------>>>
+
+		// Creating WebDriverWait object
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(DASHBOARD_HEADER_FIELD));// calling wait object
+
+		// ------->>>TimeoutException------------>>>
+		// boolean pageTitleDisplayStatus;
+		// try {
+		// WebElement DASHBOARD_HEADER_ELEMENT=driver.findElement(By.xpath("//strong[text()='Dashboard']"));
+		// pageTitleDisplayStatus = true
+		// } catch (Exception e) {
+		// pageTitleDisplayStatus = false;
+		// e.getMessage(); -----// or we can use e.getStackTrace();
+		// }
+
+		//--->> If it is false or their is Error only it we get the below message--->>
+		// Assert.assertTrue("Dashboard page not found!!", pageTitleDisplayStatus);
+	}
+
+}
